d19ac5ea1e592c235dbc9d5f438765ad93a8f3f0
Kavita108 <55924498+Kavita108@users.noreply.github.com>
2020-02-25 18:15:14 -0800

    New develop branch
    Moved code from kavita branch into develop branch

--- MediaPlayer/MusicPlayer/Properties/AssemblyInfo.cs
+++ MediaPlayer/MusicPlayer/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("mediaplayer")]
+[assembly: AssemblyTitle("MusicLibrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("HP Inc.")]
-[assembly: AssemblyProduct("mediaplayer")]
-[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MusicLibrary")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
